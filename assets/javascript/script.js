f703403655f71271f41219411ae25c69f0213fc7
SoloJett <solemansay@gmail.com>
2020-08-01 14:12:30 -0400

    I created a while loop for incorrect answers to no longer be accepted

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,31 +4,30 @@ var generateBtn = document.querySelector("#generate");
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'J', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var symbols = ['!', '@','#','$','%','^','&','*','(',')','-','_','=','+','/','~','<','>'];
+var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '/', '~', '<', '>'];
 
 
-// var passLength = parseInt(prompt('How many charcters would you like? (8-128)'));
-//       if (passLength <= 128 || passLength >= 8){
-//         alert('Password length must be 8 to 128 characters long!');
-        
-//       } 
-      // var passLength;
+var proper = false;
+while (!proper) {
+  passLength = parseInt(prompt('How many charcters would you like? (8-128)'));
+  if (passLength <= 128 && passLength >= 8) {
+    alert(passLength + "! great choice! Time to select caracter types.");
+    proper = true;
+  } else {
+    alert("Password length must be 8 to 128 characters long!");
+  }
+}
 
-      // while (passLength >= 128 || passLength <= 8) {
-        var passLength = parseInt(prompt('How many charcters would you like? (8-128)'))
-    //     if (passLength <= 128 || passLength >= 8) {
-    //         break;
-    //     }
-    //    alert("Password length must be 8 to 128 characters long!");
-      
-    // }
+var chosenCharactersArr = []
+var passwordstring = ''
 
-  var chosenCharactersArr = []
-  var passwordstring= ''
 
-if (confirm('would you like Capital letters?')){
+var select = false
+while (!select) {
+
+if (confirm('would you like Capital letters?')) {
   chosenCharactersArr.push(upperCase)
-  } 
+}
 
 if (confirm('would you like to include lowercase letters?')) {
   chosenCharactersArr.push(lowerCase)
@@ -42,22 +41,32 @@ if (confirm('would you like to include symbols?')) {
   chosenCharactersArr.push(symbols)
 }
 
-console.log(passLength, chosenCharactersArr)
+if (chosenCharactersArr.length === 1 || chosenCharactersArr.length === 2 || 
+  chosenCharactersArr.length === 3 || chosenCharactersArr.length === 4) {
+  select = true;
+} else {
+  alert("Password Must contain at least one type of character!");
+}
+}
+
+//console.log(passLength, chosenCharactersArr)
 
 
-for(var i = 0; i < passLength; i++){
+for (var i = 0; i < passLength; i++) {
   var randomCharacterType = Math.floor(Math.random() * chosenCharactersArr.length);
   var randomCharacter = Math.floor(Math.random() * chosenCharactersArr[randomCharacterType].length);
   var password = chosenCharactersArr[randomCharacterType][randomCharacter]
   passwordstring += password
 
-  console.log(randomCharacterType, randomCharacter, password, passwordstring)
+  //console.log(randomCharacterType, randomCharacter, password, passwordstring)
 }
+
+//Final password on an alert
 
 alert('Here is your secure password: ' + passwordstring)
 
 
-// Write password to the #password input
+// Function's to display final password in the text box
 
 function writePassword() {
   var password = generatePassword();
@@ -66,9 +75,11 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function generatePassword(){
+
+function generatePassword() {
   return passwordstring
 }
 
-// Add event listener to generate button
+// My event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
