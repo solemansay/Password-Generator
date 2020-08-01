47647955f699db730c8167617828e1c8c882fa62
SoloJett <solemansay@gmail.com>
2020-08-01 10:54:15 -0400

    I got the password generator to work with alerts and on page.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'J', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var symbols = ['!@#$%^&*()-_=+/`~<>.'];
+var symbols = ['!', '@','#','$','%','^','&','*','(',')','-','_','=','+','/','~','<','>'];
 
 
 // var passLength = parseInt(prompt('How many charcters would you like? (8-128)'));
@@ -24,6 +24,7 @@ var symbols = ['!@#$%^&*()-_=+/`~<>.'];
     // }
 
   var chosenCharactersArr = []
+  var passwordstring= ''
 
 if (confirm('would you like Capital letters?')){
   chosenCharactersArr.push(upperCase)
@@ -45,34 +46,29 @@ console.log(passLength, chosenCharactersArr)
 
 
 for(var i = 0; i < passLength; i++){
-  var randomUpperCase = Math.floor(Math.random() * chosenCharactersArr.length);
-  var randomLowercase = Math.floor(Math.random() * chosenCharactersArr[randomUpperCase].length);
-  // var randomNumber = Math.floor(Math.random() * chosenCharactersArr[randomUpperCase][randomLowercase].length);
-  // var randomSymbol = Math.floor(Math.random() * chosenCharactersArr[randomUpperCase][randomLowercase][randomNumber].length);
+  var randomCharacterType = Math.floor(Math.random() * chosenCharactersArr.length);
+  var randomCharacter = Math.floor(Math.random() * chosenCharactersArr[randomCharacterType].length);
+  var password = chosenCharactersArr[randomCharacterType][randomCharacter]
+  passwordstring += password
 
-  var password = chosenCharactersArr[randomUpperCase][randomUpperCase]//[randomNumber][randomSymbol]
-
-  console.log(randomUpperCase, randomLowercase, password)
+  console.log(randomCharacterType, randomCharacter, password, passwordstring)
 }
 
+alert('Here is your secure password: ' + passwordstring)
 
-
-
-// if (userWantsDigets) {
-//   choices.push(digets);
-// }
-
-// for (var i = 0; i < passLength; i++) {
-
-// }
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
-// }
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+function generatePassword(){
+  return passwordstring
+}
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
