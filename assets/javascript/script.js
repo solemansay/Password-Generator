97af97c18cc8390ec718655c178c6462292e4ec1
SoloJett <solemansay@gmail.com>
2020-07-31 22:46:34 -0400

    added some code to the JS file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,70 @@
 // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
+
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'J', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var symbols = ['!@#$%^&*()-_=+/`~<>.'];
+
+
+// var passLength = parseInt(prompt('How many charcters would you like? (8-128)'));
+//       if (passLength <= 128 || passLength >= 8){
+//         alert('Password length must be 8 to 128 characters long!');
+        
+//       } 
+      // var passLength;
+
+      // while (passLength >= 128 || passLength <= 8) {
+        var passLength = parseInt(prompt('How many charcters would you like? (8-128)'))
+    //     if (passLength <= 128 || passLength >= 8) {
+    //         break;
+    //     }
+    //    alert("Password length must be 8 to 128 characters long!");
+      
+    // }
+
+  var chosenCharactersArr = []
+
+if (confirm('would you like Capital letters?')){
+  chosenCharactersArr.push(upperCase)
+  } 
+
+if (confirm('would you like to include lowercase letters?')) {
+  chosenCharactersArr.push(lowerCase)
+}
+
+if (confirm('would you like to include numbers?')) {
+  chosenCharactersArr.push(numbers)
+}
+
+if (confirm('would you like to include symbols?')) {
+  chosenCharactersArr.push(symbols)
+}
+
+console.log(passLength, chosenCharactersArr)
+
+
+for(var i = 0; i < passLength; i++){
+  var randomUpperCase = Math.floor(Math.random() * chosenCharactersArr.length);
+  var randomLowercase = Math.floor(Math.random() * chosenCharactersArr[randomUpperCase].length);
+  // var randomNumber = Math.floor(Math.random() * chosenCharactersArr[randomUpperCase][randomLowercase].length);
+  // var randomSymbol = Math.floor(Math.random() * chosenCharactersArr[randomUpperCase][randomLowercase][randomNumber].length);
+
+  var password = chosenCharactersArr[randomUpperCase][randomUpperCase]//[randomNumber][randomSymbol]
+
+  console.log(randomUpperCase, randomLowercase, password)
+}
+
+
+
+
+// if (userWantsDigets) {
+//   choices.push(digets);
+// }
+
+// for (var i = 0; i < passLength; i++) {
+
+// }
 
 // Write password to the #password input
 // function writePassword() {
@@ -11,97 +76,3 @@
 
 // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
-
-
-//var randomstring = Math.random().toString(36).slice(-8); MIGHT HELP
-
-
-//DOM elements
-const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const numbersEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
-const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
-
-const randomFunc = {
-  lower: getRandomLower,
-  Upper: getRandomUpper,
-  number: getRandomNumber,
-  Symbol: getRandomSymbol
-};
-
-//Generate event listener
-generateEl.addEventListener('click', () => {
-  const length = lengthEl.value;
-  const hasLower = lowercaseEl.checked;
-  const hasUpper = uppercaseEl.checked;
-  const hasNumber = numbersEl.checked;
-  const hasSymbol = symbolsEl.checked;
-
-  resultEl.innerText = generatePassword(
-    hasLower, 
-    hasNumber,
-    hasUpper, 
-    hasSymbol, 
-    length
-    );
-});
-
-//Generate password function
-function generatePassword(lower, upper, number, symbol, length){
-  //1. initialize a password variable 
-  //2. filter out unchecked types
-  //3. loop over length call generator function for each type
-  //4. Add the final pw to the pw var and return
-
-  var generatePassword= '';
-
-  const typesCount = lower + upper + number + symbol;
-
-  console.log('typescount:  ', typesCount);
-
-  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter
-  (
-    item => Object.values(item)[0]
-  );
-
-  console.log('typeArr:  ', typesArr);
-
-    if(typesCount === 0) {
-      return '';
-    }
-
-    for(var i = 0; i < length; i += typesCount) {
-      typesArr.forEach(type => {
-        const funcName = Object.keys(type) [0];
-        generatePassword += randomFunc[funcName]();
-
-      });
-
-    }
-
-}
-
-//Generator function - http://www.net-comber.com/charset.html
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*+()[]{}=<>/.,';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-console.log(getRandomSymbol());
